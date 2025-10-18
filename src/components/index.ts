92d6d424b47cab86dc403e9ebcc6eb6e123bab90
MohitGeryani <mohitgeryani@gmail.com>
2025-10-19 01:47:50 +0530

    Minor Fixes v3

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,7 +5,7 @@ import {
   // ComputersCanvas,
   StarsCanvas,
 } from "./canvas";
-import Hero from "./sections/"
+import Hero from "./sections/Hero"
 import Navbar from "./layout/Navbar";
 import About from "./sections/About";
 import Tech from "./sections/Tech";
